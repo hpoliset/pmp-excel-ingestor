3ecd0e87784974215d00e44bc8a077e7dd216b65
Ramesh Ramados <rramesh@htcindia.com>
2016-01-05 21:27:22 +0530

    Code refraction for bulk upload

---
+++ src/main/java/org/srcm/heartfulness/enumeration/ExcelType.java
@@ -0,0 +1,60 @@
+package org.srcm.heartfulness.enumeration;
+
+import org.srcm.heartfulness.excelupload.transformer.ExcelDataExtractor;
+import org.srcm.heartfulness.excelupload.transformer.impl.ExcelDataExtractorV1Impl;
+import org.srcm.heartfulness.excelupload.transformer.impl.ExcelDataExtractorV2Impl;
+import org.srcm.heartfulness.validator.EventDetailsExcelValidator;
+import org.srcm.heartfulness.validator.impl.ExcelV1ValidatorImpl;
+import org.srcm.heartfulness.validator.impl.ExcelV2ValidatorImpl;
+
+/**
+ * Enumeration to identify the excel upload types used for event details upload program of Heartfulness.
+ * 
+ * @author Koustav Dutta
+ *
+ */
+public enum ExcelType {
+
+	V1(new ExcelV1ValidatorImpl(), new ExcelDataExtractorV1Impl()),					 // Denotes Excel version altered 1.0
+	V2_1 (new ExcelV2ValidatorImpl(), new ExcelDataExtractorV2Impl()),				 // Denotes Excel extractor for altered 1.0
+	INVALID(null); 																	 // Holder to display error message for invalid formats
+
+
+	/** Instance of validator to be used for the corresponding template type.  */
+	private EventDetailsExcelValidator validator;
+	
+	/** Instance of the excel parser to be used for the corresponding template type. */
+	private ExcelDataExtractor extractor;
+
+	/*
+	 * constructor
+	 */
+	private ExcelType(EventDetailsExcelValidator validator) {
+		this.validator = validator;
+	}
+
+
+	private ExcelType(EventDetailsExcelValidator validator, ExcelDataExtractor extractor) {
+		this.validator = validator;
+		this.extractor = extractor;
+	}
+
+	/**
+	 * Getter method for returning the instance of the corresponding Validator.
+	 * 
+	 * @return @see {@link EventDetailsExcelValidator}
+	 */
+	public EventDetailsExcelValidator getValidator() {
+		return validator;
+	}
+
+	/**
+	 * Getter method for returning the instance of the corresponding extractor.
+	 * 
+	 * @return @see {@link ExcelDataExtractor}
+	 */
+	public ExcelDataExtractor getExtractor() {
+		return extractor;
+	}
+
+}
---
+++ src/main/java/org/srcm/heartfulness/excelupload/transformer/ExcelDataExtractor.java
@@ -0,0 +1,22 @@
+package org.srcm.heartfulness.excelupload.transformer;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.srcm.heartfulness.model.Program;
+import org.srcm.heartfulness.util.InvalidExcelFileException;
+
+/**
+ * Extractor class to parse the excel file and populate the program and participant data.
+ * 
+ * Created by Koustav Dutta.
+ */
+public interface ExcelDataExtractor {
+	
+	/**
+	 * This method is used to get all the program details from the corresponding excel file.
+	 * 
+	 * @return Program details
+	 */
+	public Program extractExcel(Workbook workbook) throws InvalidExcelFileException ;
+
+
+}
