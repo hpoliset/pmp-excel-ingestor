fcac72bd6175f00cf34298900107a8013b604935
Goutham <goutham.y@outlook.com>
2016-02-03 15:09:39 +0530

    sms gateway - poc

--- src/main/java/org/srcm/heartfulness/webservice/SmsGatewayController.java
+++ src/main/java/org/srcm/heartfulness/webservice/SmsGatewayController.java
@@ -1,13 +1,10 @@
 package org.srcm.heartfulness.webservice;
 
-import java.util.Enumeration;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -47,21 +44,16 @@ public class SmsGatewayController {
 			@RequestParam(value = "location", required = false) String location,
 			@RequestParam(value = "carrier", required = false) String carrier,
 			@RequestParam(value = "content", required = false) String content,
-			@RequestParam(value = "location", required = false) String phoneno,
+			@RequestParam(value = "phoneno", required = false) String phoneno,
 			@RequestParam(value = "time", required = false) String time, HttpServletRequest request) {
 
-		LOGGER.debug("Request param phonecode {}", phonecode);
-		LOGGER.debug("Request param keyword {}", keyword);
-		LOGGER.debug("Request param location {}", location);
-		LOGGER.debug("Request param carrier {}", carrier);
-		LOGGER.debug("Request param content {}", content);
-		LOGGER.debug("Request param location {}", location);
-		LOGGER.debug("Request param time {}", time);
-		LOGGER.debug("Request param Method ", request.getMethod());
-		Enumeration<String> parameterEnumeration = request.getParameterNames();
-		while (parameterEnumeration.hasMoreElements()) {
-			LOGGER.debug("Paramerter {}", parameterEnumeration.nextElement());
-		}
+		LOGGER.debug("Request param -> phonecode {}", phonecode);
+		LOGGER.debug("Request param -> keyword {}", keyword);
+		LOGGER.debug("Request param -> location {}", location);
+		LOGGER.debug("Request param -> carrier {}", carrier);
+		LOGGER.debug("Request param -> content {}", content);
+		LOGGER.debug("Request param -> phoneno {}", phoneno);
+		LOGGER.debug("Request param -> time {}", time);
 		return "success";
 	}
 }
