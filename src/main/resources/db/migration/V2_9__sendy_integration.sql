f0e3f0fe632bfbe1213dc543d341cd7ba19adf21
Goutham <goutham.y@outlook.com>
2016-04-13 12:17:06 +0530

    updated sendy sql file

--- src/main/resources/db/migration/V2_9__sendy_integration.sql
+++ src/main/resources/db/migration/V2_9__sendy_integration.sql
@@ -3,8 +3,9 @@ CREATE TABLE IF NOT EXISTS `welcome_mail_subscribers` (
   `print_name` varchar(60) CHARACTER SET utf8 DEFAULT NULL,
   `email` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
   `email_sent_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `unsubscribed` tinyint(1) DEFAULT '0' COMMENT '0 means subscribed/1 means unsubscribed',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=59 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 ALTER TABLE participant ADD welcome_mail_sent tinyint(1) DEFAULT '0' COMMENT '0 means not sent/1 means sent';
 
