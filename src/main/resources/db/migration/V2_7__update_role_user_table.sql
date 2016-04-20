321f42041d2c514e8a49312c9e123c8d6223d1cb
Goutham <goutham.y@outlook.com>
2016-04-20 18:18:34 +0530

    updated scrit for user table

--- src/main/resources/db/migration/V2_7__update_role_user_table.sql
+++ src/main/resources/db/migration/V2_7__update_role_user_table.sql
@@ -1,5 +1,7 @@
+ALTER TABLE `user` MODIFY COLUMN role varchar(25) DEFAULT 'SEEKER';
+
 UPDATE `user` SET `role`='SYSTEM_ADMIN' WHERE `role`='ROLE_ADMIN';
 
 UPDATE `user` SET `role`='PRECEPTOR' WHERE `role`='ROLE_PRECEPTOR';
 
-UPDATE `user` SET `role`='SEEKER' WHERE `role`='ROLE_SEEKER';
+UPDATE `user` SET `role`='SEEKER' WHERE `role`='ROLE_SEEKER' OR `role`='';
