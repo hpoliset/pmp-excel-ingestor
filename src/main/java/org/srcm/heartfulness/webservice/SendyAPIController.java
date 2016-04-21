cf28077dcc93111f8e539014858fbef278874f22
Goutham <goutham.y@outlook.com>
2016-04-21 12:07:28 +0530

    added services for testing sendy

--- src/main/java/org/srcm/heartfulness/webservice/SendyAPIController.java
+++ src/main/java/org/srcm/heartfulness/webservice/SendyAPIController.java
@@ -4,14 +4,13 @@ import java.io.IOException;
 
 import javax.mail.MessagingException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
-import org.srcm.heartfulness.model.SendySubscriber;
 import org.srcm.heartfulness.service.WelcomeMailService;
 
 /**
@@ -25,13 +24,16 @@ public class SendyAPIController {
 
 	@Autowired
 	private WelcomeMailService sendyAPIService;
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(SendyAPIController.class);
 
 	//@Scheduled(cron = "0 18 19 * * *")
 	public void subscribeUser(){
 		try {
 			sendyAPIService.addNewSubscriber();
 		} catch (HttpClientErrorException | IOException | MessagingException e) {
-			e.printStackTrace();
+			//e.printStackTrace();
+			LOGGER.error("Exception while Subscribe - {} "+ e.getMessage());
 		}
 	}
 	//@Scheduled(cron = "0 28 19 * * *")
@@ -39,7 +41,21 @@ public class SendyAPIController {
 		try {
 			sendyAPIService.unsubscribeUsers();
 		} catch (HttpClientErrorException | IOException e) {
-			e.printStackTrace();
+			//e.printStackTrace();
+			LOGGER.error("Exception while Unsubscribe - {} "+ e.getMessage());
 		}
+	}
+	
+	@RequestMapping(value = "/testWelcomeMail", method = RequestMethod.GET)
+	public String testWelcomeMail() {
+		subscribeUser();
+		return "completed";
+	}
+	
+	@RequestMapping(value = "/testUnsubcribe", method = RequestMethod.GET)
+	public String testUnsubcribe() {
+		unsubscribeUser();
+		return "completed";
+		
 	}
 }
