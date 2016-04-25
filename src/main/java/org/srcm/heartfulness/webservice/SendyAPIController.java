a8ca659534e854f0817fc9f2a07540d7aaa15639
Goutham <goutham.y@outlook.com>
2016-04-25 20:43:19 +0530

    changed properties

--- src/main/java/org/srcm/heartfulness/webservice/SendyAPIController.java
+++ src/main/java/org/srcm/heartfulness/webservice/SendyAPIController.java
@@ -28,7 +28,7 @@ public class SendyAPIController {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(SendyAPIController.class);
 
-	@Scheduled(cron = "0 27 2 * * *")
+	@Scheduled(cron = "0 30 2 * * *")
 	public void subscribeUser(){
 		try {
 			sendyAPIService.addNewSubscriber();
