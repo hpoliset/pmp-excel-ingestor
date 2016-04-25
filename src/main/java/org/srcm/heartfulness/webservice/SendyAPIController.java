5b23036c360fbbe48a551e198769bcf0de2601df
Goutham <goutham.y@outlook.com>
2016-04-25 20:41:29 +0530

    change scheduler

--- src/main/java/org/srcm/heartfulness/webservice/SendyAPIController.java
+++ src/main/java/org/srcm/heartfulness/webservice/SendyAPIController.java
@@ -7,6 +7,7 @@ import javax.mail.MessagingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +28,7 @@ public class SendyAPIController {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(SendyAPIController.class);
 
-	//@Scheduled(cron = "0 18 19 * * *")
+	@Scheduled(cron = "0 27 2 * * *")
 	public void subscribeUser(){
 		try {
 			sendyAPIService.addNewSubscriber();
