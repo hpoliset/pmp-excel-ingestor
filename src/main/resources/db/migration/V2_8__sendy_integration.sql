9bfc45c4777a8413ecc04f590096e613dac434fb
Goutham <goutham.y@outlook.com>
2016-04-26 12:00:21 +0530

    altered migration script for sendy v2.8

--- src/main/resources/db/migration/V2_8__sendy_integration.sql
+++ src/main/resources/db/migration/V2_8__sendy_integration.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS `welcome_mail_subscribers` (
+CREATE TABLE IF NOT EXISTS `welcome_email_log` (
   `id` int(4) unsigned NOT NULL AUTO_INCREMENT,
   `print_name` varchar(60) CHARACTER SET utf8 DEFAULT NULL,
   `email` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
   `email_sent_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
-  `unsubscribed` tinyint(1)  NOT NULL DEFAULT '0' COMMENT '0 means subscribed/1 means unsubscribed',
+  `unsubscribed` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE participant ADD welcome_mail_sent tinyint(1)  NOT NULL DEFAULT '0' COMMENT '0 means not sent/1 means sent';
+ALTER TABLE participant ADD welcome_mail_sent tinyint(1) DEFAULT '0' COMMENT '0 means not sent/1 means sent';
