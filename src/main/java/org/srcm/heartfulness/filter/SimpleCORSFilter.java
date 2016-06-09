a2cc2ba313c8a91a60ab7aa53dd951d04c336507
Hima Sree  Vemuru <himasreev@COE1.HTC.LOCAL>
2016-06-09 11:34:14 +0530

    Added CORS filter

---
+++ src/main/java/org/srcm/heartfulness/filter/SimpleCORSFilter.java
@@ -0,0 +1,39 @@
+package org.srcm.heartfulness.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleCORSFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException,
+			ServletException {
+		HttpServletResponse response = (HttpServletResponse) res;
+		HttpServletRequest request = (HttpServletRequest) req;
+		response.addHeader("Access-Control-Allow-Origin", "*");
+		if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
+			response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+			response.addHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+			response.addHeader("Access-Control-Max-Age", "1");// 30 min
+		}
+			chain.doFilter(request, response);
+	}
+
+	public void init(FilterConfig filterConfig) {
+	}
+
+	public void destroy() {
+	}
+
+}
