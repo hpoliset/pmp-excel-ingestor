2912740fd5d89b4fc5d88ad058beddfe3dbe3c7a
Hima Sree  Vemuru <himasreev@COE1.HTC.LOCAL>
2016-06-13 14:31:15 +0530

    cron time and migration script change

--- src/main/resources/db/migration/V3_8__is_coordinator_informed.sql
+++ src/main/resources/db/migration/V3_8__is_coordinator_informed.sql
@@ -1,2 +1,2 @@
-ALTER TABLE participant ADD is_co_ordinator_informed TINYINT(1) DEFAULT 0;
+ALTER TABLE participant ADD is_co_ordinator_informed TINYINT(1) DEFAULT 0 NOT NULL;
 UPDATE participant SET is_co_ordinator_informed = 1 WHERE welcome_mail_sent = 1;
