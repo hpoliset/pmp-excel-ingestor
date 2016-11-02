fdd93ccd317daa1b897a2aa133c5771723d20479
Hima Sree  Vemuru <himasreev@COE1.HTC.LOCAL>
2016-11-02 14:57:39 +0530

    Changed to prod properties

--- src/main/java/org/srcm/heartfulness/constants/SMSConstants.java
+++ src/main/java/org/srcm/heartfulness/constants/SMSConstants.java
@@ -13,13 +13,13 @@ public class SMSConstants {
 	public static final String SMS_KEYWORD="LOVHFN";
 	
 	/** Holds the sub keyword for create event  */
-	public static final String SMS_CREATE_EVENT_SUB_KEYWORD="CES";
+	public static final String SMS_CREATE_EVENT_SUB_KEYWORD="CE";
 	
 	/** Holds the sub keyword for create event	 */
-	public static final String SMS_UPDATE_EVENT_SUB_KEYWORD="UES";
+	public static final String SMS_UPDATE_EVENT_SUB_KEYWORD="UE";
 	
 	/** Holds the sub keyword to get total registered users	 */
-	public static final String SMS_GET_TOTAL_REGISTERED_USERS_SUB_KEYWORD = "CRS";
+	public static final String SMS_GET_TOTAL_REGISTERED_USERS_SUB_KEYWORD = "CR";
 	
 	/** Holds the sub keyword to get total introduced users	 */
 	public static final String SMS_GET_TOTAL_REGISTERED_USERS_BY_INTRO_ID_SUB_KEYWORD = "CI";
