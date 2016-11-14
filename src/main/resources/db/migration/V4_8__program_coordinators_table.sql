ab883d643ec12ea9f14cb69dbb682ae5e923c989
Koustav Dutta Dipak Kumar Dutta <koustavd@COE1.HTC.LOCAL>
2016-11-14 13:41:08 +0530

    sql file changes

--- src/main/resources/db/migration/V4_8__program_coordinators_table.sql
+++ src/main/resources/db/migration/V4_8__program_coordinators_table.sql
@@ -5,14 +5,13 @@ CREATE TABLE `program_coordinators` (
   `coordinator_name` varchar(255) DEFAULT NULL,
   `coordinator_email` varchar(255) NOT NULL,
   `is_primary_coordinator` tinyint(1) DEFAULT 0,
-  `create_time` TIMESTAMP NOT NULL,
-   `update_time` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `update_time` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `create_time` TIMESTAMP NOT NULL ,
   PRIMARY KEY (`id`),
-  KEY `program_id` (`program_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `program_id` (`program_id`),
+  CONSTRAINT `program_coordinators_program_program_id_fk` FOREIGN KEY (`program_id`) REFERENCES `program` (`program_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `program_coordinators`
-  ADD CONSTRAINT `program_coordinators_ibfk_1` FOREIGN KEY (`program_id`) REFERENCES `program` (`program_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 CREATE TABLE IF NOT EXISTS `event_access_request` (
   `request_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -20,14 +19,12 @@ CREATE TABLE IF NOT EXISTS `event_access_request` (
   `user_id` int(4) unsigned NOT NULL,
   `status` varchar(255) NOT NULL,
   `approved_by` varchar(255) DEFAULT NULL,
-  `request_time` timestamp NOT NULL,
+  `request_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `requested_by` varchar(255) NOT NULL,
-  `approval_time` timestamp NULL DEFAULT NULL,
+  `approval_time` TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY (`request_id`),
   KEY `program_id` (`program_id`),
-  KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ;
-
-ALTER TABLE `event_access_request`
-  ADD CONSTRAINT `event_access_request_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `event_access_request_ibfk_1` FOREIGN KEY (`program_id`) REFERENCES `program` (`program_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `event_access_request_user_user_id_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `event_access_request_program_program_id_fk` FOREIGN KEY (`program_id`) REFERENCES `program` (`program_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ;
