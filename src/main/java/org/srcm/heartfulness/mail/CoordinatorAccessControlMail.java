8b2973c8602851f909978453eb371e38fd76c7e1
Koustav Dutta Dipak Kumar Dutta <koustavd@COE1.HTC.LOCAL>
2016-11-15 17:51:21 +0530

    added mailing properties

--- src/main/java/org/srcm/heartfulness/mail/CoordinatorAccessControlMail.java
+++ src/main/java/org/srcm/heartfulness/mail/CoordinatorAccessControlMail.java
@@ -3,10 +3,21 @@
  */
 package org.srcm.heartfulness.mail;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
 /**
  * @author Koustav Dutta
  *
  */
+@Component
+@ConfigurationProperties(locations = "classpath:dev.cac.mail.properties", ignoreUnknownFields = false, prefix = "mail.cac")
 public class CoordinatorAccessControlMail {
-
+	
+	private String username;
+	private String password;
+	private String hostname;
+	private String port;
+	
+	
 }
