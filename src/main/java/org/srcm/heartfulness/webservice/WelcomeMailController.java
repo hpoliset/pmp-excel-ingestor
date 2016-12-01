c6c956de9d50de326dc57a457eca268782914462
Hima Sree  Vemuru <himasreev@COE1.HTC.LOCAL>
2016-12-01 11:12:15 +0530

    Changed to dev properties for demo

--- src/main/java/org/srcm/heartfulness/webservice/WelcomeMailController.java
+++ src/main/java/org/srcm/heartfulness/webservice/WelcomeMailController.java
@@ -71,15 +71,16 @@ public class WelcomeMailController {
 	 * about the participants who have received welcome emails.It is a crob job
 	 * running at a scheduled time.
 	 */
-	@Scheduled(cron = "${welcome.mailids.coordinator.inform.cron.time}") 
+	@RequestMapping(value = "informcoordinatorswitheventdetails", method = RequestMethod.POST)
+	//@Scheduled(cron = "${welcome.mailids.coordinator.inform.cron.time}") 
 	public void sendEmailToCoordinator() {
 		LOGGER.info("START		:Cron job started to fetch participants to whom welcome mail already sent");
 		WelcomeMailService.getCoordinatorListAndSendMail();
 		LOGGER.info("END		:Cron job completed to fetch participants to whom welcome mail already sent");
 	}
 
-	//@RequestMapping(value = "informcoordinatorswithewelcomeids", method = RequestMethod.POST)
-	@Scheduled(cron = "${ewelcomeid.generate.coordinator.inform.cron.time}") 
+	@RequestMapping(value = "informcoordinatorswithewelcomeids", method = RequestMethod.POST)
+	//@Scheduled(cron = "${ewelcomeid.generate.coordinator.inform.cron.time}") 
 	public void sendGeneratedEwelcomeIdToCoordinators() {
 		LOGGER.info("START		:Cron job started to send mails to coordinator to inform participant ewelcomeid's");
 		WelcomeMailService.getGeneratedEwelcomeIdAndSendToCoordinators();
@@ -87,7 +88,7 @@ public class WelcomeMailController {
 	}
 	
 	//@RequestMapping(value = "sendwelcomemail", method = RequestMethod.POST)
-	@Scheduled(cron = "${welcome.mail.to.hfnlist.cron.time}") 
+	/*@Scheduled(cron = "${welcome.mail.to.hfnlist.cron.time}") */
 	public void sendWelcomeMail() {
 		try {
 			LOGGER.info("Sending mail to hfn list called.");
