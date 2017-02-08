81c92c9722f71526999fc4fe9e7b99ca61fe475a
Hima Sree Vemuru <himasreev@htcindia.com>
2017-02-08 18:02:41 +0530

    Changed to prod properties

--- src/main/java/org/srcm/heartfulness/webservice/EWelcomeIDGenerationScheduler.java
+++ src/main/java/org/srcm/heartfulness/webservice/EWelcomeIDGenerationScheduler.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RestController;
 import org.srcm.heartfulness.constants.EmailLogConstants;
 import org.srcm.heartfulness.constants.ErrorConstants;
@@ -54,7 +55,7 @@ public class EWelcomeIDGenerationScheduler {
 	/**
 	 * Cron to generate EWelcomeIDs for the participants.
 	 */
-	/*@Scheduled(cron = "${welcome.mailids.generation.cron.time}") */
+	@Scheduled(cron = "${welcome.mailids.generation.cron.time}") 
 	public void generateEWelcomeIDsForParticipants() {
 		LOGGER.info("START : CRON : EWELCOMEID GENERATION : Scheduler to generate EwelcomeID's for the participants started at - "
 				+ new Date());
