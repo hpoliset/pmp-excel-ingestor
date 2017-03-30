79a39342c861bae58e7e5ce99420f01f452c49eb
Koustav <koustav.dipak@htcindia.com>
2017-03-30 18:19:42 +0530

    committed for welcome id generation code

--- src/main/java/org/srcm/heartfulness/webservice/EWelcomeIDGenerationScheduler.java
+++ src/main/java/org/srcm/heartfulness/webservice/EWelcomeIDGenerationScheduler.java
@@ -57,8 +57,7 @@ public class EWelcomeIDGenerationScheduler {
 	/**
 	 * Cron to generate EWelcomeIDs for the participants.
 	 */
-	/*@Scheduled(cron = "${welcome.mailids.generation.cron.time}") */
-	@RequestMapping(value = "/testwelcomeidgeneration", method = RequestMethod.POST)
+	@Scheduled(cron = "${welcome.mailids.generation.cron.time}") 
 	public void generateEWelcomeIDsForParticipants() {
 		LOGGER.info("START : CRON : EWELCOMEID GENERATION : Scheduler to generate EwelcomeID's for the participants started at - "
 				+ new Date());
