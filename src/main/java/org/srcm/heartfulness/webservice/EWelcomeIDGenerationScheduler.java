a694cf2804ce36eec9846de7e2c3bc61fcaf1eb9
Koustav <koustav.dipak@htcindia.com>
2017-03-30 18:23:33 +0530

    welcome id generation

--- src/main/java/org/srcm/heartfulness/webservice/EWelcomeIDGenerationScheduler.java
+++ src/main/java/org/srcm/heartfulness/webservice/EWelcomeIDGenerationScheduler.java
@@ -7,8 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.srcm.heartfulness.constants.EmailLogConstants;
 import org.srcm.heartfulness.constants.ErrorConstants;
