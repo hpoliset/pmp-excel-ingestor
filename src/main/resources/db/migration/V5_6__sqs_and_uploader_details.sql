45dd19958ef54316c81286162cde543ceaa0564e
Koustav <koustav.dipak@htcindia.com>
2017-06-13 11:49:33 +0530

    changed userid and uploader id as default 0

--- src/main/resources/db/migration/V5_6__sqs_and_uploader_details.sql
+++ src/main/resources/db/migration/V5_6__sqs_and_uploader_details.sql
@@ -1,5 +1,5 @@
 --ALTER TABLE program ADD sqs_push_status TINYINT(1) DEFAULT 0;
 ALTER TABLE user MODIFY address VARCHAR(255) DEFAULT NULL;
 ALTER TABLE uploaded_files ADD status varchar(50) NULL;
-ALTER TABLE program ADD user_id int(4) UNSIGNED NULL;
-ALTER TABLE program ADD uploaded_file_id int(4) UNSIGNED NULL;
+ALTER TABLE program ADD user_id int(4) UNSIGNED DEFAULT 0;
+ALTER TABLE program ADD uploaded_file_id int(4) UNSIGNED DEFAULT 0;
